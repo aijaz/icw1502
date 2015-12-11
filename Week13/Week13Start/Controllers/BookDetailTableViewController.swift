a306efae4b7bf4d1c638464a1d2463770e8ce61b
Aijaz A. Ansari <aijaz@enoor.com>
2015-12-10 23:21:37 -0600

    Created Week13Start project

---
+++ Week13/Week13Start/Controllers/BookDetailTableViewController.swift
@@ -0,0 +1,121 @@
+//
+//  BookDetailTableViewController.swift
+//  TableViewBooks
+//
+//  Created by Aijaz Ansari on 11/21/15.
+//  Copyright © 2015 Euclid Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+typealias CompletionHandler = (Book)->()
+
+class BookDetailTableViewController: UITableViewController {
+
+    var book:Book?
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var pagesLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    var completionHandler: CompletionHandler?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        if let book = book {
+            titleLabel.text = book.title
+            authorLabel.text = book.author
+            pagesLabel.text = "\(book.pages)"
+            languageLabel.text = book.language.rawValue
+            
+        }
+        navigationItem.title = "Detail"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        completionHandler?(book!)
+    }
+    
+    func done () {
+        completionHandler?(book!)
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: - Table view data source
+
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "EditPagesSegue" {
+            let numVC = segue.destinationViewController as! NumericEntryViewController
+            numVC.initialValue = book!.pages
+            numVC.completionHandler = { np in
+                self.book?.pages = np
+                self.pagesLabel.text = "\(np)"
+            }
+        }
+    }
+
+
+}
---
+++ Week13/Week13Start/Controllers/NumericEntryTableViewController.swift
@@ -0,0 +1,49 @@
+//
+//  NumericEntryTableViewController.swift
+//  TableViewBooks
+//
+//  Created by Aijaz Ansari on 11/21/15.
+//  Copyright © 2015 Euclid Software, LLC. All rights reserved.
+//
+
+import UIKit
+
+typealias entryHandler = (Int)->()
+
+class NumericEntryViewController: UIViewController {
+
+    var completionHandler: entryHandler?
+    var initialValue:Int?
+    
+    @IBOutlet weak var textField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        if let initialValue = initialValue {
+            textField.text = "\(initialValue)"
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        var numPages = 1
+        if let pagesString = textField.text {
+            if let numPagesUnwrapped = Int(pagesString) {
+                numPages = numPagesUnwrapped
+            }
+        }
+        completionHandler?(numPages)
+    }
+
+    
+}
